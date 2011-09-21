e50afb9d79f06d84cb1fc4c8335256219e63dac0
Michael J. Flickinger <mjflick@gnu.org>
2011-09-21 19:37:09 -0400

    define functions &  #include "alloc.h"

--- include/fstack.h
+++ include/fstack.h
@@ -10,3 +10,16 @@ typedef struct {
   int size;
 } fstack;
 
+
+void fstack_init (fstack *);
+int fstack_push (fstack *, void *);
+int fstack_unshift (fstack *, void *);
+int fstack_length (fstack *);
+fstack_item *fstack_peek_item (fstack *, int);
+void *fstack_peek (fstack *, int);
+void *fstack_pop (fstack *);
+void *fstack_shift (fstack *);
+void *fstack_shift_peek (fstack *);
+void *fstack_pop_peek (fstack *);
+int  fstack_insert (fstack *, int, void *);
+void *fstack_delete (fstack *, int);
